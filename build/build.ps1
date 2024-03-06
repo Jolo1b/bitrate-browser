038f685fa76f14750134ed115172687187645f9e
JoelBartok <JoloBartok1b@gmail.com>
2024-03-06 21:24:02 +0100

    build script is fixed

--- build/build.ps1
+++ build/build.ps1
@@ -2,33 +2,21 @@ Set-Location "$PSScriptRoot\..\Scripts"
 
 $items = Get-ChildItem -Recurse -Filter *.ps1
 
-foreach($item in $items) {
-    $content = Get-Content $item.Name 
-    $files = Get-ChildItem
-    $global:goodline = $false
-    
-    
-    "" | Out-file "$PSScriptRoot\dist\bitrate-browser.ps1"
-    
-    
-    foreach($line in $content){
-        foreach($file in $files){
-            if($line -like ". `"`$PSScriptRoot\$($file.Name)`""){
-                Write-Host $line
-                Get-Content $file.FullName | Out-file "$PSScriptRoot\dist\bitrate-browser.ps1" -Append
-                $global:goodline = $true        
-            }
-        }
-    
-        if(-not $global:goodline) {
-            $line | Out-file "$PSScriptRoot\dist\bitrate-browser.ps1" -Append 
-        } 
-        else {
-            $global:goodline = $false
-        }
+"" | Out-file "$PSScriptRoot\dist\bitrate-browser.ps1"
+$mainFileContent = Get-Content "$PSScriptRoot\..\Scripts\Main.ps1"
+
+$i = 0
+while($i -le 3){
+    foreach($item in $items) {
+        Write-Host "-> $([char]0x001b)[32m$($item.FullName)"
+        $content = Get-Content $item.FullName -Raw
+
+        $mainFileContent = $mainFileContent.Replace(". `"`$PSScriptRoot\$($item.Name)`"", $content)
     }
+    $i++
 }
 
+$mainFileContent | Set-Content "$PSScriptRoot\dist\bitrate-browser.ps1"
 Set-Location $PSScriptRoot
 
 ps12exe -noConsole -noOutput -inputFile .\dist\bitrate-browser.ps1 -outputFile ".\dist\bitrate browser_x86.exe" -noError -architecture x86 -noVisualStyles -iconFile ..\Assets\Icon.ico
