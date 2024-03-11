10bb4fdee2d673729eb4386f2bb482e014b09728
JoelBartok <JoloBartok1b@gmail.com>
2024-03-11 16:48:56 +0100

    improved build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,9 +1,13 @@
 Set-Location "$PSScriptRoot\..\Scripts"
-
 $items = Get-ChildItem -Recurse -Filter *.ps1
 
 "" | Out-file "$PSScriptRoot\dist\bitrate-browser.ps1"
 $mainFileContent = Get-Content "$PSScriptRoot\..\Scripts\Main.ps1"
+
+$red = [char]0x001b + "[31m"
+$noColor = [char]0x001b + "[0m"
+
+Write-Host $red"Building..."$noColor
 
 $i = 0
 while($i -le 3){
@@ -19,5 +23,11 @@ while($i -le 3){
 $mainFileContent | Set-Content "$PSScriptRoot\dist\bitrate-browser.ps1"
 Set-Location $PSScriptRoot
 
+Write-Host $red"Compiling..."$noColor
+
 ps12exe -noConsole -noOutput -inputFile .\dist\bitrate-browser.ps1 -outputFile ".\dist\bitrate browser_x86.exe" -noError -architecture x86 -noVisualStyles -iconFile ..\Assets\Icon.ico
 ps12exe -noConsole -noOutput -inputFile .\dist\bitrate-browser.ps1 -outputFile ".\dist\bitrate browser_x64.exe" -noError -architecture x64 -noVisualStyles -iconFile ..\Assets\Icon.ico
+ps12exe -noConsole -noOutput -inputFile .\dist\bitrate-browser.ps1 -outputFile ".\dist\bitrate browser_anycpu.exe" -noError -architecture anycpu -noVisualStyles -iconFile ..\Assets\Icon.ico
+
+Write-Host $red"Press any key to continue..."$noColor -NoNewline
+[System.Console]::ReadKey()
